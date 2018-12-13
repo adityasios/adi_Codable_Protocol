880c76af565d00af65a983a3682deebdf90a3408
Rakhi <rakhibhardwaj@rakhis-macbook-pro.local>
2018-12-13 23:46:02 +0530

    Initial Commit

---
+++ CodableProtocol/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodableProtocol
+//
+//  Created by Rakhi on 13/12/18.
+//  Copyright © 2018 Rakhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
