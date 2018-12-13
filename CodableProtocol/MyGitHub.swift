22a2bdb06bf04a51e06f38d4be1c9dc9ddafbbe3
Rakhi <rakhibhardwaj@rakhis-macbook-pro.local>
2018-12-13 23:58:52 +0530

    1.First Commit

---
+++ CodableProtocol/MyGitHub.swift
@@ -0,0 +1,27 @@
+//
+//  MyGitHub.swift
+//  CodableProtocol
+//
+//  Created by Rakhi on 13/12/18.
+//  Copyright © 2018 Rakhi. All rights reserved.
+//
+
+import UIKit
+
+struct MyGitHub : Codable{
+   
+    let name: String?
+    let location: String?
+    let followers: Int?
+    let avatarUrl: URL?
+    let repos: Int?
+    
+    private enum GitKeys : String,CodingKey {
+        case name
+        case location
+        case followers
+        case repos = "public_repos"
+        case avatarUrl = "avatar_url"
+        
+    }
+}
